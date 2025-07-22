fa0f0adfa87a409fbed336e69f1392bc62b48a23
Peter Mai <peter.mai1996@gmail.com>
2025-07-22 11:49:24 -0500

    Updated the endpoint in order to get food

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  // const choice = document.querySelector('input').value
+  const url = 'https://foodish-api.com/api/'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
